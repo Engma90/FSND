b979629af0558156d4fdd2e5c51082fe9a1679a5
Mohammad Ahmad <eng.ma90@gmail.com>
2020-12-18 16:42:20 +0200

    Added id column to show for allowing duplicates

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -40,10 +40,10 @@ migrate = Migrate(app, db)
 
 class Show(db.Model):
     __tablename__ = 'Show'
+    id = db.Column(db.Integer, primary_key=True)
     artist_id = db.Column(db.Integer, db.ForeignKey(
-        'Artist.id'), primary_key=True)
-    venue_id = db.Column(db.Integer, db.ForeignKey(
-        'Venue.id'), primary_key=True)
+        'Artist.id'), nullable=False)
+    venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'), nullable=False)
     start_time = db.Column(db.DateTime, nullable=False)
 
     artist = db.relationship("Artist", back_populates="venues")
