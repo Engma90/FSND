94f3f9291472ed66a24c3bce4a43068ae170952f
Mohammad Ahmad <eng.ma90@gmail.com>
2020-12-19 23:28:29 +0200

    Marked todo as done

--- projects/01_fyyur/starter_code/config.py
+++ projects/01_fyyur/starter_code/config.py
@@ -9,6 +9,5 @@ DEBUG = True
 # Connect to the database
 
 
-# TODO IMPLEMENT DATABASE URL #
 SQLALCHEMY_DATABASE_URI = 'postgres://postgres@localhost:5432/fyyur'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
