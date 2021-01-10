2449967f16fd45ee0db9c71b1d1302b502dcda7c
Mohammad Ahmad <eng.ma90@gmail.com>
2021-01-10 23:03:56 +0200

    Checked by pycodestyle tool

--- projects/02_trivia_api/starter/backend/flaskr/__init__.py
+++ projects/02_trivia_api/starter/backend/flaskr/__init__.py
@@ -1,9 +1,6 @@
-import os
 from flask import Flask, request, abort, jsonify
-from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql import func
 from flask_cors import CORS
-import random
 
 from models import setup_db, Question, Category
 
@@ -27,10 +24,11 @@ def create_app(test_config=None):
     setup_db(app)
 
     '''
-    @TODO:# Set up CORS. Allow '*' for origins. Delete the sample route after completing the TODOs
+    @TODO:# Set up CORS. Allow '*' for origins. Delete the sample route after
+    completing the TODOs
     '''
     # CORS(app)
-    cors = CORS(app, resources={r"/*": {"origins": "*"}})
+    CORS(app, resources={r"/*": {"origins": "*"}})
     '''
     @TODO:# Use the after_request decorator to set Access-Control-Allow
     '''
@@ -77,7 +75,8 @@ def create_app(test_config=None):
 
     TEST: At this point, when you start the application
     you should see questions and categories generated,
-    ten questions per page and pagination at the bottom of the screen for three pages.
+    ten questions per page and pagination at the
+    bottom of the screen for three pages.
     Clicking on the page numbers should update the questions.
     '''
     @app.route('/questions')
@@ -103,7 +102,8 @@ def create_app(test_config=None):
     @TODO:
     Create an endpoint to DELETE question using a question ID.
 
-    TEST: When you click the trash icon next to a question, the question will be removed.
+    TEST: When you click the trash icon next to a question, the question
+    will be removed.
     This removal will persist in the database and when you refresh the page.
     '''
     @app.route('/questions/<int:question_id>', methods=['DELETE'])
@@ -131,19 +131,19 @@ def create_app(test_config=None):
     category, and difficulty score.
 
     TEST: When you submit a question on the "Add" tab,
-    the form will clear and the question will appear at the end of the last page
-    of the questions list in the "List" tab.
+    the form will clear and the question will appear at the end of the last
+    page of the questions list in the "List" tab.
     '''
 
     '''
-    @TODO: 
-    Create a POST endpoint to get questions based on a search term. 
-    It should return any questions for whom the search term 
-    is a substring of the question. 
+    @TODO:
+    Create a POST endpoint to get questions based on a search term.
+    It should return any questions for whom the search term
+    is a substring of the question.
 
-    TEST: Search by any phrase. The questions list will update to include 
-    only question that include that string within their question. 
-    Try using the word "title" to start. 
+    TEST: Search by any phrase. The questions list will update to include
+    only question that include that string within their question.
+    Try using the word "title" to start.
     '''
 
     @app.route('/questions', methods=['POST'])
@@ -174,21 +174,21 @@ def create_app(test_config=None):
                 return jsonify({
                     'success': True
                 })
-        except:
+        except Exception as ex:
             abort(422)
 
     '''
-    @TODO: 
-    Create a GET endpoint to get questions based on category. 
+    @TODO:
+    Create a GET endpoint to get questions based on category.
 
-    TEST: In the "List" tab / main screen, clicking on one of the 
-    categories in the left column will cause only questions of that 
-    category to be shown. 
+    TEST: In the "List" tab / main screen, clicking on one of the
+    categories in the left column will cause only questions of that
+    category to be shown.
     '''
     @app.route('/categories/<int:category_id>/questions')
     def retrieve_questions_by_category(category_id):
         selection = Question.query.filter(
-            Question.category == category_id).order_by(Question.id).all()
+            Question.category == str(category_id)).order_by(Question.id).all()
         current_questions = paginate_questions(request, selection)
 
         if len(current_questions) == 0:
@@ -202,15 +202,15 @@ def create_app(test_config=None):
         })
 
     '''
-    @TODO: 
-    Create a POST endpoint to get questions to play the quiz. 
-    This endpoint should take category and previous question parameters 
-    and return a random questions within the given category, 
-    if provided, and that is not one of the previous questions. 
+    @TODO:
+    Create a POST endpoint to get questions to play the quiz.
+    This endpoint should take category and previous question parameters
+    and return a random questions within the given category,
+    if provided, and that is not one of the previous questions.
 
     TEST: In the "Play" tab, after a user selects "All" or a category,
     one question at a time is displayed, the user is allowed to answer
-    and shown whether they were correct or not. 
+    and shown whether they were correct or not.
     '''
 
     @app.route('/quizzes', methods=['POST'])
@@ -222,7 +222,8 @@ def create_app(test_config=None):
             if quiz_category['id'] != 0:
                 question = Question.query.filter(
                     Question.category == quiz_category['id']).filter(
-                    Question.id.notin_(previous_questions)).order_by(func.random()).first()
+                    Question.id.notin_(previous_questions)).order_by(
+                        func.random()).first()
             else:
                 question = Question.query.order_by(func.random()).filter(
                     Question.id.notin_(previous_questions)).first()
@@ -235,13 +236,13 @@ def create_app(test_config=None):
                 'question': question
             })
 
-        except:
+        except Exception as ex:
             abort(422)
 
     '''
-    @TODO: 
-    Create error handlers for all expected errors 
-    including 404 and 422. 
+    @TODO:
+    Create error handlers for all expected errors
+    including 404 and 422.
     '''
     @app.errorhandler(404)
     def not_found(error):
